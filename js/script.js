a50bca9507b1b14379e422d0e182d99dbcc88855
DinazavriK <avedenko.a@mail.ru>
2017-08-21 22:01:09 +0300

    First Commit

---
+++ js/script.js
@@ -0,0 +1,53 @@
+;
+(function ($) {
+
+	$(document).ready(function () {
+
+		var $gridImg = $('.isotope').isotope({
+			itemSelector: ".item",
+			layoutMode: 'masonry',
+			masonry: {
+				columnWidth: 270,
+				gutter: 5
+			}
+		});
+
+		$("#filters button").click(function () {
+			var $this = $(this);
+			if (!$this.hasClass("is-checked")) {
+				$this.parents("#options").find(".is-checked").removeClass("is-checked");
+				$this.addClass("is-checked");
+			}
+			var selector = $this.attr("data-filter");
+			$gridImg.isotope({
+				filter: selector
+			});
+		});
+
+
+		$('a[href*=#]:not([href=#])').click(function () {
+			if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') ||
+				location.hostname == this.hostname) {
+
+				var target = $(this.hash);
+				target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+				if (target.length) {
+					$('html,body').animate({
+						scrollTop: target.offset().top
+					}, 800);
+				}
+			}
+		});
+
+		$('.team__slider').slick({
+			dots: true
+		});
+
+		$('.testimonials__slider').slick({
+			dots: true,
+			autoplay: true,
+			autoplaySpeed: 4000
+		});
+
+	});
+})(jQuery);
